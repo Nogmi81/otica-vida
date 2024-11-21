a483e55a0b92d45066fd746da5a938dc952e45e8
André Tavares <nogmi81@gmail.com>
2024-11-21 18:36:50 -0300

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'; // Substitua pelo caminho correto do seu arquivo CSS
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Conteudo from '@/componentes/conteudo';
+import Topo from '@/componentes/Topo';
+import Rodape from '@/componentes/Rodape';
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      
+      <Topo />
+      <Conteudo />
+      <Rodape />
+      
+    </>
+  );
 }
+
+export default App;
