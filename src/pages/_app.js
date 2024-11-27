0e26e84b5cc098be564cfb5fa13ebe0678efcb0c
André Tavares <140923882+Nogmi81@users.noreply.github.com>
2024-11-27 18:09:29 -0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'; // Substitua pelo caminho correto do seu arquivo CSS
 
-import Conteudo from '@/componentes/conteudo';
+import Conteudo from '@/componentes/Conteudo';
 import Topo from '@/componentes/Topo';
 import Rodape from '@/componentes/Rodape';
 
